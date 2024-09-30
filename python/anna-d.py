a59f0a356fc139ff584dfa994da4aa4cbec18350
dudnikovaanna545@gmail.com~ <dudnikovaanna545@gmail.com>
2024-09-30 21:19:59 +0300

    Changes commited in anna-d

---
+++ python/anna-d.py
@@ -0,0 +1,35 @@
+# name = input("What is your name?")
+# surname = input("What is your surname?")
+# town = input("Where do you live?")
+# period = input("How long have you liver here?")
+# print("Hello, " + name + " " + surname + "! You`ve lived in " + town + " for " + period + " years. ")
+
+# a1 = int(input("Please enter first number"))
+# a2 = int(input("Please enter second number"))
+# sum = a1 + a2
+# dif = a1 - a2
+# mult = a1 * a2
+# div = a1 / a2
+# print("Summary: " + str(sum) + "\n" + "Difference: " + str(dif)+ "\n" + "Multiplication: " + str(mult)+ "\n" + "Division: "+ str(div))
+
+# first_digit = float(input("Please enter a digit"))
+# second_digit = float(input("Please enter another digit"))
+# digits_div = first_digit / second_digit
+# round_div = round(digits_div, 3)
+# print(round_div)
+
+# one = int(input("Enter a number: "))
+# two = int(input("Enter one more number: "))
+# print(one > two)
+
+# one = float(input("Enter a number: "))
+# two = float(input("Enter one more number: "))
+# print(one == two)
+
+print("Let`s calculate the discriminant together!")
+b = int(input("Enter b"))
+a = int(input("Enter a"))
+c = int(input("Enter c"))
+D = b*b - 4*a*c
+print("The discriminant is", D)
+
