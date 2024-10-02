90a406e3412fe1a2d4f9ef30393d08503ea08a47
Maryna Marchuk <mmarchuk81@gmail.com>
2024-10-02 13:41:27 -0700

    Update maryna-m-lesson-7-elif-and.py with and or not

--- Python/maryna-m-lesson-7-elif-and.py
+++ Python/maryna-m-lesson-7-elif-and.py
@@ -29,3 +29,52 @@
 # if eyecolor == 3:
 #     print("My eyecolor is blue.")
 
+#and or
+# score = int(input("Enter the score:"))
+# if score > 7 and score < 10:
+#     print (f'between 7 and 10')
+# score = int(input("Enter the score:"))
+# if score == 7 or score == 10:
+#     print (f'Your score is 7 or 10')
+
+#And
+# True True = True
+# True False = False
+# False True = False
+# False False = False
+# If I enter admin and 1347 only then it will print Access granted = True True = True,
+# otherwise it will be false and use else
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+#
+# # Check if the username is "admin" or the password is "1357"
+# if username == "admin" or password == "1357":
+#     print("Access granted!")
+# else:
+#     print("Access denied!")
+
+# or
+# True True = True
+# True False = True
+# False True = False
+#
+# age = 45
+# if age < 18 or age > 60:
+#     print("You are not in the working age group.")
+# else:
+#     print("You are in the working age group.")
+
+# not
+# False=True
+# True=False
+# a = 33
+# b = 200
+# if not a > b:
+#   print("a is NOT greater than b")
+#
+#   number = 15
+#  if not (number >= 10 and number <= 20):
+#       print("The number is NOT within the range of 10 to 20.")
+#   else:
+#       print("The number is within the range or 10 to 20.")
+
