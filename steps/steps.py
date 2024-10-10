37772a3830770d88ffeb67f8a0328e0ab92fc8c1
Andrey <baykov.andrey@gmail.com>
2024-10-09 23:47:15 -0700

    lesson 10 prepared

--- steps/steps.py
+++ steps/steps.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+
 import api
 
 
